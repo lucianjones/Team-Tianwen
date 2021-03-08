c1955bcddab7950302abe18e3d83d49380f5237c
Lucian Jones <lucianjones96@gmail.com>
2021-03-08 14:16:10 -0500

    began user register route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const { csrfProtection, asyncHandler } = require("../utils");
+const db = require("../db/models");
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/register", csrfProtection, function (req, res, next) {
+  const user = db.User.build();
+  res.render("user-register", {
+    title: "Register",
+    csrfToken: req.csrfToken(),
+  });
 });
 
 module.exports = router;
