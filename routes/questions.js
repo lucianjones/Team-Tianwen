b5f8fa97405ea27441ad2c0ffca44c3ee91f05e4
Heriberto Montoya <hmontoya3821@gmail.com>
2021-03-10 13:08:26 -0600

    styled the individual questions page

--- routes/questions.js
+++ routes/questions.js
@@ -10,6 +10,7 @@ router.get('/', asyncHandler(async(req, res) => {
   const questions = await db.Question.findAll({
     limit: 10
   })
+  console.log(questions)
   res.render('questions', {questions})
 }))
 
