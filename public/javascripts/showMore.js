532e606b6449f124978258d92a780423a9c776ed
Heriberto Montoya <hmontoya3821@gmail.com>
2021-03-11 14:33:32 -0600

    fixed the show more feature

--- public/javascripts/showMore.js
+++ public/javascripts/showMore.js
@@ -6,12 +6,26 @@ window.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector(".container");
     const entirePage = document.querySelector(".entirePage");
     let count = 10;
+    let heightContainer = 1500
+    let tlength = document.querySelectorAll(".qquestion").length
     showMore.addEventListener('click', (e) => {
-      const questionTitle = document.querySelectorAll(".q");
-      for(let i = 0; i < count; i++) {
-        questionTitle[i].style.position='absolute'
+      const questionTitle = document.querySelectorAll(".qquestion");
+      console.log(tlength)
+      if(tlength > 10){
+        for(let i = 0; i < count; i++) {
+          questionTitle[i].style.position='absolute'
+        }
+        tlength-=10
+        count+=10;
+        heightContainer+= 1500
+        container.style.height = heightContainer + 'px'
+      }else {
+        for(let i = 0; i < questionTitle.length; i++) {
+          questionTitle[i].style.position='absolute'
+        }
+        container.style.height = '100%'
       }
-      count+=10;
+
     })
 
 })
