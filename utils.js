b8b4712064b3c8237230782a7087afe79ae7a26b
Heriberto Montoya <hmontoya3821@gmail.com>
2021-03-14 19:18:11 -0500

    errorr msg for when passwords do not match

--- utils.js
+++ utils.js
@@ -38,7 +38,18 @@ const userValidator = [
         .withMessage('Password cannot exceed 255 characters')
         .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/, 'g')
         .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, and number'),
-];
+    check('confirmPassword')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Confirm Password')
+        .isLength({ max: 50 })
+        .withMessage('Confirm Password must not be more than 50 characters long')
+        .custom((value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Confirm Password does not match Password');
+          }
+          return true;
+        }),
+    ];
 
 
 const loginValidators = [
